72f35a169b93eb1afab3cfaf5b71051ce4da3b1f
Jonathan Keane <jkeane@gmail.com>
2019-12-02 08:25:23 -0600

    first commit

---
+++ R/one_func.R
@@ -0,0 +1,11 @@
+#' One function
+#'
+#' A noop function that returns its only input.
+#'
+#' @param x the thing to be returned
+#'
+#' @return `x`, the input
+#' @export
+one_function <- function(x) {
+    return(x)
+}
