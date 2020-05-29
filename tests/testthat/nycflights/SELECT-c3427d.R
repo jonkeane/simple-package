c63aa8fddb2586269b9eda2df5aaf32a8fb18bb6
Jonathan Keane <jkeane@gmail.com>
2020-05-29 17:49:45 -0500

    With tests now

---
+++ tests/testthat/nycflights/SELECT-c3427d.R
@@ -0,0 +1,2 @@
+structure(list(carrier = "VX", name = "Virgin America"), class = "data.frame", row.names = c(NA, 
+-1L))
--- tests/testthat/test-one_func.R
+++ tests/testthat/test-one_func.R
@@ -1,5 +1,9 @@
-test_that("get an airline", {
-    one_airline <- get_an_airline()
-    expect_s3_class(one_airline, "data.frame")
-    expect_equal(nrow(one_airline), 1)
+with_mock_db({
+    test_that("get an airline", {
+        one_airline <- get_an_airline()
+        expect_s3_class(one_airline, "data.frame")
+        expect_equal(nrow(one_airline), 1)
+        expect_equal(one_airline$carrier, "VX")
+        expect_equal(one_airline$name, "Virgin America")
+    })
 })
