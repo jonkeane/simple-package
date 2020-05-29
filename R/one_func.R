304a9cc8b0756fe900ba8da22165391bbe73d260
Jonathan Keane <jkeane@gmail.com>
2020-05-29 17:00:55 -0500

    indentation

--- R/one_func.R
+++ R/one_func.R
@@ -5,19 +5,19 @@
 #' @return a dataframe with airlines from the DB
 #' @export
 get_an_airline <- function(n = 1) {
-    con <- DBI::dbConnect(RPostgres::Postgres(), dbname = "nycflights")
-    on.exit(DBI::dbDisconnect(con))
+  con <- DBI::dbConnect(RPostgres::Postgres(), dbname = "nycflights")
+  on.exit(DBI::dbDisconnect(con))
 
-    query <- paste0(
-        "SELECT carrier, name FROM rpostgres.airlines ",
-        " ORDER BY random() LIMIT ",
-        n
-    )
+  query <- paste0(
+    "SELECT carrier, name FROM rpostgres.airlines ",
+    " ORDER BY random() LIMIT ",
+    n
+  )
 
-    out <- DBI::dbGetQuery(con, query)
+  out <- DBI::dbGetQuery(con, query)
 
-    if (out$name == "Testy McAirline") {
-        stop("This is the test airline \U1F643")
+  if (out$name == "Testy McAirline") {
+    stop("This is the test airline \U1F643")
   }
 
   return(out)
