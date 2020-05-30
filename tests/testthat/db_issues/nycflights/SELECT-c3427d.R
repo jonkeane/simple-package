95251d9efc05f47abda913c7f5e1e289dd345833
Jonathan Keane <jkeane@gmail.com>
2020-05-30 12:07:20 -0500

    Add db_issues fixture

---
+++ tests/testthat/db_issues/nycflights/SELECT-c3427d.R
@@ -0,0 +1,2 @@
+structure(list(carrier = "NA", name = "Testy McAirline"), class = "data.frame", row.names = c(NA,
+-1L))
--- tests/testthat/test-one_func.R
+++ tests/testthat/test-one_func.R
@@ -7,3 +7,11 @@ with_mock_db({
         expect_equal(one_airline$carrier, "HA")
     })
 })
+
+with_mock_path(test_path("db_issues"), {
+    with_mock_db({
+        test_that("get_an_airline errors on tests", {
+            expect_error(get_an_airline())
+        })
+    })
+})
