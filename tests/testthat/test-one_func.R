f4cfa88890e296017168366383ad7f6f44b27d81
Jonathan Keane <jkeane@gmail.com>
2021-10-16 18:01:28 -0500

    LOLOL

--- tests/testthat/test-one_func.R
+++ tests/testthat/test-one_func.R
@@ -1,16 +1,9 @@
-public({
-    test_that("ignoring the error also works", {
-        expect_equal(one_function("one"), "one")
-        expect_equal(one_function("two"), "two")
-    })
-
-    test_that("expect the error works", {
-        expect_equal(one_function("one"), "one")
-        expect_error(one_function())
-    })
+test_that("ignoring the error also works", {
+    expect_equal(one_function("one"), "one")
+    expect_equal(one_function("two"), "two")
 })
 
-
-
-
-
+test_that("expect the error works", {
+    expect_equal(one_function("one"), "one")
+    expect_error(one_function())
+})
